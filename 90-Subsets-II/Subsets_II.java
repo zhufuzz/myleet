0ab3fbe34408474e1f487b6a5bdd2e3d7ac3e135
zhufuzz <zhufuzz@hotmail.com>
2016-08-19 17:02:03 -0400

    [{Subsets_II}][{AC}]committed by BRUCE

---
+++ 90-Subsets-II/Subsets_II.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+     List<List<Integer>> result = new ArrayList<>();
+     if (nums == null || nums.length == 0){
+         return result;
+     }
+     Arrays.sort(nums);
+     List<Integer> subset = new ArrayList<>();
+     helper(result, subset, nums, 0);
+     return result;
+    }
+    private void helper(List<List<Integer>> result,
+                        List<Integer> subset,
+                        int[] nums, int fromIndex){
+        //REMEMBER THIS!!! clone the subset before use.
+        result.add(new ArrayList(subset));
+        for(int i = fromIndex; i < nums.length; i++){
+            if(i != fromIndex && nums[i] == nums[i - 1]){
+                continue;
+            }
+            subset.add(nums[i]);
+            helper(result, subset, nums, i + 1);
+            subset.remove(subset.size() - 1);
+        }
+    }
+}
