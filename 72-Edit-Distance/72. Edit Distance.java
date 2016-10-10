196b539936b4510debcb65b3b8784ef8145e3113
zhufuzz <zhufuzz@hotmail.com>
2016-10-09 21:36:52 -0400

    [72. Edit Distance][Compile Error]committed by BRUCE

---
+++ 72-Edit-Distance/72. Edit Distance.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        
+        int[][] f = new int[m + 1][n + 1];
+        
+        for (int i = 0; i < m + 1; i++) {
+            f[i][0] = i; 
+        }
+        
+        for (int j = 0; j < n + 1; j++) {
+            f[0][j] = i;
+        }
+        
+        for (int i = 0; i < m + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    f[i][j] = f[i - 1][j + 1] + 1;
+                } else {
+                    f[i][j] = Math.max(Math.max(f[i - 1][j], f[i][j - 1]), f[i - 1][j - 1]);
+                }
+            }
+        }
+    }
+}
